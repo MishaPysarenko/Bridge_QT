838ac991662d1585303734ef7891979405802c3f
unknown <pmsmihsa@gmail.com>
2023-02-21 12:32:35 +0200

    new class, client and server. And edit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include "server.h"
+#include "client.h"
 
 int main(int argc, char *argv[])
 {
@@ -13,5 +15,7 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
+
+
     return app.exec();
 }
